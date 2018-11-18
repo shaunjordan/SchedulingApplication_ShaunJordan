155722d4e5ecf4f6b81d5e4ff6dba9a56d61551e
Shaun Jordan <s1bravo@gmail.com>
2018-11-18 13:49:38 -0600

    quick updates

--- SchedulingUI/LoginForm.cs
+++ SchedulingUI/LoginForm.cs
@@ -11,24 +11,29 @@ using MySql.Data.MySqlClient;
 using System.Threading;
 using System.Globalization;
 using SchedulingUI.Properties;
+using System.Device.Location;
 
 namespace SchedulingUI
 {
     public partial class LoginForm : Form
     {
-        
+
+        GeoCoordinateWatcher userLocation = new GeoCoordinateWatcher();
         public LoginForm()
         {
             InitializeComponent();
-            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
 
-            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
-            MessageBox.Show(Properties.strings.Hello);
+
+            //Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE");
+            //MessageBox.Show(Properties.strings.Hello);
+
+            MessageBox.Show(userLocation.Position.Location.ToString());
+
             //MySqlConnection connection = new MySqlConnection();
 
             //connection.Open();
