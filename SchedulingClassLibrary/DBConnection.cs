50051c7bc7b0d9ef8a5dd05f0e7b7a505e8a8a62
Shaun Jordan <s1bravo@gmail.com>
2018-12-04 22:31:15 -0600

    updates

--- SchedulingClassLibrary/DBConnection.cs
+++ SchedulingClassLibrary/DBConnection.cs
@@ -1,11 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace SchedulingClassLibrary
 {
     public class DBConnection
     {
-        
+        private MySqlConnection connection;
+        private string server;
+        private string databaseName;
+        private string user;
+        private string password;
+        private string connectionString;
+
+        public void InitConnection()
+        {
+            server = "52.206.157.109";
+            databaseName = "U05Fdz";
+            user = "U05Fdz";
+            password = "53688484989";
+
+            connectionString = String.Format("server={0}; database={1}; username={2}; password={3}", server, databaseName, user, password);
+
+            connection = new MySqlConnection(connectionString);
+
+            connection.Open(); //add System.Data as reference
+        }
+
+        private void Connection()
+        {
+            try
+            {
+                //connection.Open();
+
+            }
+            catch
+            {
+
+            }
+        }
     }
 }
--- SchedulingUI/LoginForm.cs
+++ SchedulingUI/LoginForm.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 using System.Threading;
 using System.Globalization;
 using SchedulingUI.Properties;
